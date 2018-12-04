d8aaf2d847a317c01c64b928a08939365bac5f19
Urmil Parikh <uparikh@ncsu.edu>
2018-12-03 19:54:12 -0500

    final working

--- Client.java
+++ Client.java
@@ -1,158 +1,176 @@
 import java.io.*;
 import java.net.*;
-import java.nio.file.Files;
+import java.nio.file.*;
 import java.nio.ByteBuffer;
 
-class Client {
+public class Client {
+	public static void main(String[] args) throws IOException {
+		String hostname = "";
+		int port = 0;
+		String filename= "";
+		int N = 0;
+		int MSS = 0;
+		if(args.length >= 5 ){
+			hostname = args[0];
+			port = Integer.parseInt(args[1]);
+			filename = args[2];
+			N = Integer.parseInt(args[3]);
+			MSS = Integer.parseInt(args[4]);
+		} else {
+			System.out.println("Insufficient Arguments");
+			System.exit(0);
+		}
 
-   public static void main(String args[]) throws Exception {
-      // Initialize Sender
-      String hostname = "";
-      int port = 0;
-      String filepath = "";
-      int N = 0;
-      int MSS = 0;
-      if(args.length >= 5 ){
-         hostname = args[0];
-         port = Integer.parseInt(args[1]);
-         filepath = args[2];
-         N = Integer.parseInt(args[3]);
-         MSS = Integer.parseInt(args[4]);
-      } else {
-         System.out.println("Insufficient Arguments");
-         System.exit(0);
-      }
-      
-      // Buffer data being read from files in byte array
-      byte[] buffer = readFile(filepath);
+		// Create client socket
+		DatagramSocket clientSocket = null;
+		try {
+			clientSocket = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
 
-      // Generate Datagram Packets for buffer
-      byte[][] packets = generatePackets(buffer, MSS);
-
-      // Get IP address of Receiver from the hostname
-      InetAddress IPAddress = null;
-      try {
+	  	// Get IP address of Receiver from the hostname
+		InetAddress IPAddress = null;
+		try {
 			IPAddress = InetAddress.getByName(hostname);
 		} catch (UnknownHostException e) {
-         System.out.println("Host not found!");
-         System.exit(0);
-      }
+			System.out.println("Host not found!");
+			System.exit(0);
+		}
 
-      // Maintain a window index to know the current index of window until where ACK received
-      DatagramSocket clientSocket = new DatagramSocket();
-      int window_index = 0;
-      while(window_index < packets.length) {
+		// Buffer data being read from files in byte array
+		byte[] buffer = readFile(filename);
 
-         // Send packets without ack for the remaining window size
-         int idx = window_index;
-         int remaining_window_size = N;
-         while(remaining_window_size-- > 0 && idx < packets.length) {
-            DatagramPacket sendPacket = new DatagramPacket(packets[idx], packets[idx].length, IPAddress, port);
-            System.out.println("packets sent = " + idx);
-            idx++;
-            clientSocket.send(sendPacket);
-         }
-         
-         // Receive an ACK and set timeout for 1000 milliseconds
-         byte[] receiveData = new byte[MSS + 8];
-         int timeout = 1000;
-         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-         try {
-            clientSocket.setSoTimeout(timeout);
-            clientSocket.receive(receivePacket);
-            int ack_seq_no = ProcessACK(receivePacket.getData());
-            if(ack_seq_no == window_index){
-               System.out.println("ACK index = " + window_index);
-               window_index++;
-            }
-         } catch (SocketTimeoutException ste) {
-            // TIMEOUT!! Do nothing.
-            System.out.println("Timeout, sequence number = " + window_index);
-         }
+		// Generate Datagram Packets for buffer
+		byte[][] packets = generatePackets(buffer, MSS);
 
-      }
-      // send EOF packet to indicate that file transfer is complete
-      // byte[] eof = new byte[8];
-      // DatagramPacket eofPacket = new DatagramPacket(eof, eof.length, IPAddress, port);
-      // clientSocket.send(eofPacket);
+		// Maintain a window index to know the current index of window until where ACK received
+		int idx = 0;
+		int pointer = 0;
+		int seqAck = -1;
 
-      // clientSocket.close();
-   }
+		long start = System.currentTimeMillis();
+		while ((idx * MSS) < buffer.length) {
+			// send packets - multiple at once
+			while (pointer < N && (idx* MSS) < buffer.length) {
+				DatagramPacket sendPacket = new DatagramPacket(packets[idx], packets[idx].length, IPAddress, port);
+				try {
+					clientSocket.send(sendPacket);
+					System.out.println("Sending packet Seq No : " + idx);
+					idx++;
+					pointer++;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			// receive ACK
+			byte[] receive = new byte[MSS + 8];
+			DatagramPacket receivePacket = new DatagramPacket(receive, receive.length);
+			boolean flag = true;
 
-   public static byte[] readFile(String path){
-      byte[] a = null;
-      try {
-         a = Files.readAllBytes(new File(path).toPath());
-      } catch(Exception e) {
-         System.out.println("No such file exists!");
-         System.exit(0);
-      }
-      return a;
-   }
+			int tempIndex = idx;
+			try {
+				clientSocket.setSoTimeout(1000);
+				while (flag) {
+					clientSocket.receive(receivePacket);
+					seqAck = ProcessACK(receivePacket.getData());
+					System.out.println("ACK received for Seq No : " + seqAck);
+					if (seqAck == tempIndex - 1) { 
+						// if proper seq no received
+						pointer = 0;
+						idx = tempIndex;
+						flag = false;
+					} else if (seqAck != -1) {
+						pointer = idx- seqAck - 1;
+						idx = seqAck + 1;
+					}
+				}
+			} catch (SocketTimeoutException e) {
+				// timeout occurs
+				System.out.println("Timeout, sequence number = " + seqAck);
+				idx= seqAck + 1;
+				pointer = 0;
+			}
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken: " + (end - start));
+	}
 
-   public static byte[] checksum(byte[] data) {
-      // checksum calculation. Ref - StackOverflow
-      int sum = 0;
-      for(int i = 0; i < data.length;i++){
-          sum += (data[i]&0xff) << 8;
-          if(++i == data.length) break;
-          sum += (data[i]&0xff);
-      }
-      sum = (~((sum & 0xffff)+(sum >> 16)))&0xffff;
-      byte[] buffer = ByteBuffer.allocate(4).putInt(sum).array();
-      // return only last 2 bytes of int as the first 2 are not used
-      // checksum is 16 bits = 2 bytes
-      byte[] res = new byte[2];
-      res[0] = buffer[2];
-      res[1] = buffer[3];
-      return res;
-   }
-
-   public static byte[][] generatePackets(byte[] data, int MSS) {
-      int start_idx, end_idx, idx;
-      start_idx = end_idx = idx = 0;
-      byte[][] packets = new byte[data.length/MSS + 1][];
-
-      while(end_idx < data.length){
-         end_idx += MSS;
-         if(end_idx > data.length) end_idx = data.length;
-
-         // get packet data
-         byte[] packet_data = new byte[end_idx - start_idx];
-         System.arraycopy(data, start_idx, packet_data, 0, packet_data.length);
-         // generate Header fields for packet
-         // 4 byte seq number
-         byte[] seq_no = ByteBuffer.allocate(4).putInt(idx).array();
-         // 2 byte checksum
-         byte[] chksum = checksum(packet_data);
-         // 2 byte fixed value for data packet
-         short number = 21845;
-         byte[] identifier = ByteBuffer.allocate(2).putShort(number).array();
-
-         // concatenate header and data
-         byte[] packet = new byte[seq_no.length + chksum.length + identifier.length + packet_data.length];
-         System.arraycopy(seq_no, 0, packet, 0, seq_no.length);
-         System.arraycopy(chksum, 0, packet, seq_no.length, chksum.length);
-         System.arraycopy(identifier, 0, packet, seq_no.length + chksum.length, identifier.length);
-         System.arraycopy(packet_data, 0, packet, seq_no.length + chksum.length + identifier.length, packet_data.length);
-
-         // store it in packet list
-         packets[idx] = packet;
-
-         idx++;
-         start_idx = end_idx;
-      }
-
-      return packets;
-   }
-
-   public static int ProcessACK(byte[] data) {
-      // check if it's an ACK by checking its identifier field
-      if(data[6] != -43 && data[7] != 86) return -1;
-      // get the sequence number from header of packet
-      byte[] seq_no = new byte[4];
-      System.arraycopy(data, 0, seq_no, 0, seq_no.length);
-      return ByteBuffer.wrap(seq_no).getInt();
-   }
+	public static byte[] readFile(String path){
+		byte[] a = null;
+		try {
+		   a = Files.readAllBytes(new File(path).toPath());
+		} catch(Exception e) {
+		   System.out.println("No such file exists!");
+		   System.exit(0);
+		}
+		return a;
+	 }
+  
+	public static byte[] checksum(byte[] data) {
+		// checksum calculation. Ref - StackOverflow
+		int sum = 0;
+		for(int i = 0; i < data.length;i++){
+			sum += (data[i]&0xff) << 8;
+			if(++i == data.length) break;
+			sum += (data[i]&0xff);
+		}
+		sum = (~((sum & 0xffff)+(sum >> 16)))&0xffff;
+		byte[] buffer = ByteBuffer.allocate(4).putInt(sum).array();
+		// return only last 2 bytes of int as the first 2 are not used
+		// checksum is 16 bits = 2 bytes
+		byte[] res = new byte[2];
+		res[0] = buffer[2];
+		res[1] = buffer[3];
+		return res;
+	}
+  
+	 public static byte[][] generatePackets(byte[] data, int MSS) {
+		int start_idx, end_idx, idx;
+		start_idx = end_idx = idx = 0;
+		byte[][] packets = new byte[data.length/MSS + 1][];
+  
+		while(end_idx < data.length){
+		   end_idx += MSS;
+		   if(end_idx > data.length) end_idx = data.length;
+  
+		   // get packet data
+		   byte[] packet_data = new byte[end_idx - start_idx];
+		   System.arraycopy(data, start_idx, packet_data, 0, packet_data.length);
+		   // generate Header fields for packet
+		   // 4 byte seq number
+		   byte[] seq_no = ByteBuffer.allocate(4).putInt(idx).array();
+		   // 2 byte checksum
+		   byte[] chksum = checksum(packet_data);
+		   // 2 byte fixed value for data packet
+		   short number = 21845;
+		   byte[] identifier = ByteBuffer.allocate(2).putShort(number).array();
+  
+		   // concatenate header and data
+		   byte[] packet = new byte[seq_no.length + chksum.length + identifier.length + packet_data.length];
+		   System.arraycopy(seq_no, 0, packet, 0, seq_no.length);
+		   System.arraycopy(chksum, 0, packet, seq_no.length, chksum.length);
+		   System.arraycopy(identifier, 0, packet, seq_no.length + chksum.length, identifier.length);
+		   System.arraycopy(packet_data, 0, packet, seq_no.length + chksum.length + identifier.length, packet_data.length);
+  
+		   // store it in packet list
+		   packets[idx] = packet;
+  
+		   idx++;
+		   start_idx = end_idx;
+		}
+  
+		return packets;
+	 }
+  
+	public static int ProcessACK(byte[] data) {
+		// check if it's an ACK by checking its identifier field
+		if(data[6] != -43 && data[7] != 86) return -1;
+		// get the sequence number from header of packet
+		byte[] seq_no = new byte[4];
+		System.arraycopy(data, 0, seq_no, 0, seq_no.length);
+		return ByteBuffer.wrap(seq_no).getInt();
+	}
 
 }
--- Server.java
+++ Server.java
@@ -1,110 +1,127 @@
 import java.io.*;
 import java.net.*;
-import java.nio.file.Files;
+import java.nio.file.*;
 import java.nio.ByteBuffer;
 
-class Server {
-   public static void main(String args[]) throws Exception {
-      // Initialize Receiver
-      int port = 0;
-      String filepath = "";
-      double p = 0.0;
-      if(args.length >= 3){
-         port = Integer.parseInt(args[0]);
-         filepath = args[1];
-         p = Double.parseDouble(args[2]);
-      } else {
-         System.out.println("Insufficient Arguments");
-         System.exit(0);
-      }
-      // Initialize the file writer
-      FileOutputStream f = null;
-      try{
-         f = new FileOutputStream(filepath);
-      } catch(Exception e) {
-         e.printStackTrace();
-      }
-      // Initialize Datagram Server at the port
-      DatagramSocket serverSocket = null;
+public class Server {
+
+	public static void main(String[] args) {
+		int port = 0;
+		String filename = "";
+		double probablity = 0.0;
+		if(args.length >= 3){
+			port = Integer.parseInt(args[0]);
+			filename = args[1];
+			probablity = Double.parseDouble(args[2]);
+		} else {
+			System.out.println("Insufficient Arguments");
+			System.exit(0);
+		}
+
+		// Initialize the file writer
+		FileOutputStream f = null;
+		try{
+			f = new FileOutputStream(filename);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Initialize the sequence index
+		int idx = 0;
+
+		// Initialize Datagram Server at the port
+		DatagramSocket serverSocket = null;
 		try {
 			serverSocket = new DatagramSocket(port);
-		} catch (Exception e) {
+		} catch (SocketException e) {
 			e.printStackTrace();
-      }
-      // Initialize the sequence index
-      int idx = 0;
+		}
 
-      // Listen to request from sender
-      while(true) {
-         try {
-            byte[] buffer = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            serverSocket.receive(packet);
-            // get packet content
-            byte[] packet_content = packet.getData();
-            // get sequence number
-            byte[] seq_no_buf = new byte[4];
-            System.arraycopy(packet_content, 0, seq_no_buf, 0, seq_no_buf.length);
-            int seq_no = ByteBuffer.wrap(seq_no_buf).getInt();
-            // Drop packet based on probablity
-            if(Math.random() < p) {
-               System.out.println("Packet loss, sequence number = " + seq_no);
-               continue;
-            }
-            // get data from packet
-            byte[] data = new byte[packet.getLength() - 8];
-            System.arraycopy(packet_content, 8, data, 0, data.length);
-            // generate ACK only if
-            // 1. checksum is valid
-            // 2. sequence is in order
-            // 3. is a data packet
-            if((checksum(data)[0] == packet_content[4] && checksum(data)[1] == packet_content[5]) && (idx == seq_no) && (packet_content[6] == 85 && packet_content[7] == 85)) {
-               InetAddress clientIPAddress = packet.getAddress();
-               int clientPort = packet.getPort();
-               byte[] sendData = generateACK(seq_no);
-               DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientIPAddress, clientPort);
-               serverSocket.send(sendPacket);
-               // increment idx which is the next expected seq no
-               idx++;
-               // write the data to file
-               f.write(data);
-            }
+		// Listen to request from sender
+		DatagramPacket packet = null;
+		boolean flag = true;
+		while (flag) {
+			try {
+				byte[] buffer = new byte[1024];
+				packet = new DatagramPacket(buffer, buffer.length);
+				try {
+					if(idx == 0){
+						serverSocket.setSoTimeout(60000);
+					} else {
+						serverSocket.setSoTimeout(5000);
+					}
+					serverSocket.receive(packet);
+				} catch (SocketTimeoutException e) {
+					// timeout occurs
+					System.out.println("Timeout!!! Transfer Complete");
+					break;
+				}
 
-         } catch(Exception e) {
-            e.printStackTrace();
-            break;
-         }
-      }
-   }
+				// get packet content
+				byte[] packet_content = packet.getData();
+				// get sequence number
+				byte[] seq_no_buf = new byte[4];
+				System.arraycopy(packet_content, 0, seq_no_buf, 0, seq_no_buf.length);
+				int seq_no = ByteBuffer.wrap(seq_no_buf).getInt();
+				// Drop packet based on probablity
+				if (Math.random() <= probablity) {
+					System.out.println("Packet loss, sequence number = " + seq_no);
+					continue;
+				}
+				// get data from packet
+				byte[] data = new byte[packet.getLength() - 8];
+				System.arraycopy(packet_content, 8, data, 0, data.length);
+				// generate ACK only if
+				// 1. checksum is valid
+				// 2. sequence is in order
+				// 3. is a data packet
+				if((checksum(data)[0] == packet_content[4] && checksum(data)[1] == packet_content[5]) && (idx == seq_no) && (packet_content[6] == 85 && packet_content[7] == 85)) {
+					// write the data to file
+					f.write(data);
+					// Generate ACK
+					InetAddress IP_addr = packet.getAddress();
+					int portNumber = packet.getPort();
+					byte[] acknowledgement = generateACK(seq_no);
+					packet = new DatagramPacket(acknowledgement, acknowledgement.length, IP_addr, portNumber);
+					serverSocket.send(packet);
+					System.out.println("ACK sent for: " + seq_no);
+					// Increment index
+					idx++;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		serverSocket.close();
+	}
 
-   public static byte[] checksum(byte[] data) {
-      // checksum calculation. Ref - StackOverflow
-      int sum = 0;
-      for(int i = 0; i < data.length;i++){
-          sum += (data[i]&0xff) << 8;
-          if(++i == data.length) break;
-          sum += (data[i]&0xff);
-      }
-      sum = (~((sum & 0xffff)+(sum >> 16)))&0xffff;
-      byte[] buffer = ByteBuffer.allocate(4).putInt(sum).array();
-      // return only last 2 bytes of int as the first 2 are not used
-      // checksum is 16 bits = 2 bytes
-      byte[] res = new byte[2];
-      res[0] = buffer[2];
-      res[1] = buffer[3];
-      return res;
-   }
-
-   public static byte[] generateACK(int seq_no) {
-      byte[] header = new byte[8];
-      byte[] seq_no_buf = ByteBuffer.allocate(4).putInt(seq_no).array();
-      System.arraycopy(seq_no_buf, 0, header, 0, seq_no_buf.length);
-      // binary to decimal conversion for 1010101010101010
-      header[4] = 0;
-      header[5] = 0;
-      header[6] = -43;
-      header[7] = 86;
-      return header;
-   }
-
+	public static byte[] checksum(byte[] data) {
+		// checksum calculation. Ref - StackOverflow
+		int sum = 0;
+		for(int i = 0; i < data.length;i++){
+			sum += (data[i]&0xff) << 8;
+			if(++i == data.length) break;
+			sum += (data[i]&0xff);
+		}
+		sum = (~((sum & 0xffff)+(sum >> 16)))&0xffff;
+		byte[] buffer = ByteBuffer.allocate(4).putInt(sum).array();
+		// return only last 2 bytes of int as the first 2 are not used
+		// checksum is 16 bits = 2 bytes
+		byte[] res = new byte[2];
+		res[0] = buffer[2];
+		res[1] = buffer[3];
+		return res;
+	}
+  
+	public static byte[] generateACK(int seq_no) {
+		byte[] header = new byte[8];
+		byte[] seq_no_buf = ByteBuffer.allocate(4).putInt(seq_no).array();
+		System.arraycopy(seq_no_buf, 0, header, 0, seq_no_buf.length);
+		// binary to decimal conversion for 1010101010101010
+		header[4] = 0;
+		header[5] = 0;
+		header[6] = -43;
+		header[7] = 86;
+		return header;
+	}
 }
